39d4765e3879c31f0cf6817c0ba7a1ba07fe50f9
Miles Eastwood <MEastwood@spartaglobal.com>
2019-10-09 19:03:41 +0100

    Managed to automate the instalation of nginx and nodejs

--- environment/provision.sh
+++ environment/provision.sh
@@ -4,10 +4,10 @@
 sudo apt-get update -y
 
 # Code to automate installation of nginx
-sudo apt update
-sudo apt install nginx
+sudo apt update -y
+sudo apt install nginx -y
 
 # Code to automate installation nodejs
-sudo apt-get install curl
+sudo apt-get install curl -y
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install nodejs
+sudo apt-get install nodejs -y
